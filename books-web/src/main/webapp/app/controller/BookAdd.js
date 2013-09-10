89131aa7b716b0f2c42b6b93a00a34c73fc147b4
jendib <benjamin.gam@gmail.com>
2013-09-10 20:21:20 +0200

    Redirect to the book once added

--- books-web/src/main/webapp/app/controller/BookAdd.js
+++ books-web/src/main/webapp/app/controller/BookAdd.js
@@ -3,7 +3,7 @@
 /**
  * Book add controller.
  */
-App.controller('BookAdd', function($scope, $http, $stateParams, Restangular) {
+App.controller('BookAdd', function($scope, $http, $state, $stateParams, Restangular) {
   /**
    * Open barcode scanner with a return URL on this page.
    */
@@ -14,14 +14,13 @@ App.controller('BookAdd', function($scope, $http, $stateParams, Restangular) {
   };
 
   /**
-   * Add a book manually from ISBN.
+   * Add a book manually from an ISBN number.
    */
   $scope.addBook = function() {
     Restangular.one('book').put({
       isbn: $scope.isbn
-    }).then(function() {
-          $scope.isbn = '';
-          alert('Book added');
+    }).then(function(data) {
+          $state.transitionTo('bookview', { id: data.id });
         }, function(response) {
           alert(response.data.message);
         });
